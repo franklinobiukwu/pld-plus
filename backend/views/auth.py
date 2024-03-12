fa9956a8bb2f84b3fb148304ff765bd95eafa181
MDIMACat <romanmdima@gmail.com>
2024-03-12 21:11:26 +0200

    Created a simple backend set up using flask with all important dependencies

---
+++ backend/views/auth.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+auth_blueprint = Blueprint('auth', __name__)
+
+
+#ROUTES FOR AUTHORISATION
+@auth_blueprint.route('/auth', methods=['GET'])
+def hello_auth():
+    return {"message":'Hello Auth'}
